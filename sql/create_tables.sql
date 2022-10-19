f0113ffeb80d75c56abe55262bd8de3c0aa33452
cyberian <fox4080@gmail.com>
2022-10-19 14:28:36 +0700

    Исправлены небольшие проблемы с остуствием NOT NULL

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS genre
 (
     genre_id UUID DEFAULT uuid_generate_v4(),
-    name TEXT,
+    name TEXT NOT NULL,
     PRIMARY KEY (genre_id)
 );
 
 CREATE TABLE IF NOT EXISTS movie
 (
     movie_id UUID DEFAULT uuid_generate_v4(),
-    name TEXT,
+    name TEXT NOT NULL,
     poster TEXT,
     description TEXT,
     year INT,
@@ -24,8 +24,8 @@ CREATE TABLE IF NOT EXISTS movie
 
 CREATE TABLE IF NOT EXISTS movie_films
 (
-    movie_id UUID,
-    genre_id UUID,
+    movie_id UUID NOT NULL,
+    genre_id UUID NOT NULL,
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
     FOREIGN KEY (genre_id) REFERENCES genre(genre_id),
     UNIQUE (movie_id, genre_id)
@@ -33,15 +33,18 @@ CREATE TABLE IF NOT EXISTS movie_films
 
 CREATE TYPE gender AS ENUM ('male', 'female');
 
+CREATE TYPE user_role AS ENUM ('user', 'admin', 'moderator');
+
 CREATE TABLE IF NOT EXISTS users
 (
     user_id UUID DEFAULT uuid_generate_v4(),
-    name TEXT,
-    birth_date DATE,
-    username TEXT,
+    name TEXT NOT NULL,
+    birth_date DATE NOT NULL,
+    username TEXT NOT NULL,
     email TEXT CHECK (email ~ '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$' ),
-    password TEXT,
-    gender gender,
+    password TEXT NOT NULL,
+    gender gender NOT NULL,
+    user_role user_role NOT NULL,
     PRIMARY KEY (user_id),
     UNIQUE (username),
     UNIQUE (email)
@@ -49,8 +52,8 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS favorites
 (
-    user_id UUID,
-    movie_id UUID,
+    user_id UUID NOT NULL,
+    movie_id UUID NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
     UNIQUE (user_id, movie_id)
@@ -59,11 +62,11 @@ CREATE TABLE IF NOT EXISTS favorites
 CREATE TABLE IF NOT EXISTS review
 (
     review_id UUID DEFAULT uuid_generate_v4(),
-    user_id UUID,
-    movie_id UUID,
+    user_id UUID NOT NULL,
+    movie_id UUID NOT NULL,
     rating INT CHECK ( rating <= 10 AND rating >= 1 ),
-    is_anonymous BOOLEAN,
-    create_date_time DATE,
+    is_anonymous BOOLEAN NOT NULL,
+    create_date_time DATE NOT NULL,
     PRIMARY KEY (review_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
@@ -72,9 +75,9 @@ CREATE TABLE IF NOT EXISTS review
 
 CREATE TABLE IF NOT EXISTS jwt_tokens
 (
-    token TEXT,
-    create_date DATE,
-    user_id UUID,
+    token TEXT NOT NULL,
+    create_date DATE NOT NULL,
+    user_id UUID NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     UNIQUE (token)
 )
