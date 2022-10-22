203c82b4360b74c547b1c8adbf62cd998a266ec0
cyberian <fox4080@gmail.com>
2022-10-22 12:26:01 +0700

    Написан логгер.

---
+++ utils/logger.php
@@ -0,0 +1,21 @@
+<?php
+function log_in_file($status, $message): void
+{
+    $log = date('Y-m-d H:i:s') . ' ' . $status . ' ' . $message;
+    file_put_contents(__DIR__ . '/../log.txt', $log . PHP_EOL, FILE_APPEND);
+}
+
+function log_info($message): void
+{
+    log_in_file('[INFO]', $message);
+}
+
+function log_warning($message): void
+{
+    log_in_file('[WARN]', $message);
+}
+
+function log_err($message): void
+{
+    log_in_file('[ERROR]', $message);
+}
