574ec6c0b29dbeb3a9eb130ebe8762f2b000d753
cyberian <fox4080@gmail.com>
2022-10-25 08:46:05 +0700

    Изменения под использование модели, а не DTO

--- utils/database/DBRequest.php
+++ utils/database/DBRequest.php
@@ -14,17 +14,17 @@ function query_to_db($connect, $text)
     return pg_fetch_object(pg_query($connect,'SELECT * FROM jwt_tokens'), 3);
 }
 
-function add_user($connect, UserRegisterModel $userRegisterModel)
+function add_user($connect, User $user)
 {
     $query = 'INSERT INTO users(name, birth_date, username, email, password, gender, user_role) VALUES ($1, $2, $3, $4, $5, $6, $7);';
 
     $params = [];
-    $params[1] = $userRegisterModel->name;
-    $params[2] = $userRegisterModel->birthDate;
-    $params[3] = $userRegisterModel->userName;
-    $params[4] = $userRegisterModel->email;
-    $params[5] = $userRegisterModel->password;
-    $params[6] = $userRegisterModel->gender === 1 ? 'male' : 'female';
+    $params[1] = $user->getName();
+    $params[2] = $user->getBirthDate();
+    $params[3] = $user->getUsername();
+    $params[4] = $user->getEmail();
+    $params[5] = $user->getPassword();
+    $params[6] = $user->getGender() == 1 ? 'male' : 'female';
     $params[7] = 'user';
     $result = pg_query_params($connect, $query, $params);
 
