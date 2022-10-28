bb5a626a010a5280d0ed109353d2b986d4ce1432
cyberian <fox4080@gmail.com>
2022-10-28 15:56:28 +0700

    Добавлен endpoint удаления ревью.

--- api/movie/{uuid}/review/{uuid}/delete.php
+++ api/movie/{uuid}/review/{uuid}/delete.php
@@ -1,5 +1,20 @@
 <?php
 function route($request)
 {
-    setHTTPStatus(501, 'delete method');
+    try{
+        $connect = connect();
+        $jwt = new JWT(from_token($request->getToken()));
+        if(validate_JWT($connect, $jwt))
+        {
+            delete_review($connect, $request->getParams()['id']);//TODO возможно стоит по id фильма
+        }
+        else
+        {
+            setHTTPStatus(401);
+        }
+    }
+    catch (Exception $e) {
+        echo $e->getMessage();
+        setHTTPStatus(503);//TODO нормальные ошибки.
+    }
 }
